c696233f10d5a5f302e9a05db242907e67d6196e
valery-barysok <valery.barysok@gmail.com>
2015-04-29 23:39:29 +0300

    work example that use session-file-store.

---
+++ example/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+app.use(session({
+    store: new FileStore(),
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.get('/', function (req, res) {
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>views: ' + req.session.views + '</p>');
+    res.end();
+  } else {
+    req.session.views = 1;
+    res.end('welcome to the file session demo. refresh!');
+  }
+});
+
+var server = app.listen(1337, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
