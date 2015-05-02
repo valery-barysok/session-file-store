f7f107f944334ec7e2ac6745ae22eaefff1dff75
valery-barysok <valery.barysok@gmail.com>
2015-05-02 18:19:12 +0300

    reap and store tests were updated. reap, destroy and destroyIfExpired functions were updated.

---
+++ test/reap.js
@@ -0,0 +1,68 @@
+var helpers = require('../lib/session-file-helpers'),
+  chai = require('chai'),
+  expect = chai.expect,
+  fs = require('fs-extra'),
+  os = require('os'),
+  path = require('path'),
+  childProcess = require('child_process'),
+  clone = require('lodash.clone');
+
+describe('reap', function () {
+  var SESSIONS_PATH = path.join(os.tmpdir(), 'sessions');
+  var SESSION_ID = 'session_id';
+  var SESSION = {
+    cookie: {
+      originalMaxAge: null,
+      expires: null,
+      httpOnly: true,
+      path: '/'
+    },
+    views: 9,
+    __lastAccess: 1430336255633
+  };
+
+  var NOOP_FN = function () {
+  };
+  var SESSIONS_OPTIONS = helpers.defaults({
+    path: SESSIONS_PATH,
+    logFn: NOOP_FN
+  });
+
+  var EXPIRED_SESSION_ID = 'expired_' + SESSION_ID;
+  var EXPIRED_SESSION_FILE = path.join(SESSIONS_OPTIONS.path, EXPIRED_SESSION_ID + '.json');
+  var expiredSession = clone(SESSION);
+  expiredSession.__lastAccess = 0;
+
+  var SESSION_FILE = path.join(SESSIONS_OPTIONS.path, SESSION_ID + '.json');
+  var session = clone(SESSION);
+  session.__lastAccess = new Date().getTime();
+
+  beforeEach(function (done) {
+    fs.emptyDir(SESSIONS_OPTIONS.path, function () {
+      fs.writeJson(EXPIRED_SESSION_FILE, expiredSession, function () {
+        fs.writeJson(SESSION_FILE, session, done);
+      });
+    });
+  });
+
+  afterEach(function (done) {
+    fs.remove(SESSIONS_OPTIONS.path, done);
+  });
+
+  it('should remove stale session file', function (done) {
+    helpers.reap(SESSIONS_OPTIONS, function () {
+      expect(err).to.not.exist;
+      done();
+    });
+  });
+
+  it('should remove stale session file in the distinct process', function (done) {
+    childProcess.execFile('./reap-worker.js', [SESSIONS_OPTIONS.path, SESSIONS_OPTIONS.ttl], {
+      cwd: path.join(process.cwd(), 'lib')
+    }, function (err, stdout, stderr) {
+      expect(err).to.not.exist;
+      done();
+    });
+  });
+});
+
--- test/store.js
+++ test/store.js
@@ -1,3 +1,8 @@
+var helpers = require('../lib/session-file-helpers'),
+  fs = require('fs-extra'),
+  os = require('os'),
+  path = require('path');
+
 var session = function () {
 };
 session.Store = function () {
@@ -5,11 +10,27 @@ session.Store = function () {
 var FileStore = require('../lib/session-file-store')(session);
 
 describe('store', function () {
+  var SESSIONS_PATH = path.join(os.tmpdir(), 'sessions');
+
+  var NOOP_FN = function () {
+  };
+  var SESSIONS_OPTIONS = helpers.defaults({
+    path: SESSIONS_PATH,
+    logFn: NOOP_FN
+  });
 
   describe('#constructor', function () {
 
+    before(function (done) {
+      fs.remove(SESSIONS_OPTIONS.path, done);
+    });
+
+    after(function (done) {
+      fs.remove(SESSIONS_OPTIONS.path, done);
+    });
+
     it('should construct', function () {
-      var store = new FileStore({});
+      var store = new FileStore(SESSIONS_OPTIONS);
     });
   });
 });
