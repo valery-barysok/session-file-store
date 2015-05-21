49f6cebd6d73c853a6f96e1da36141ef04461aee
valery-barysok <valery.barysok@gmail.com>
2015-05-21 16:35:24 +0300

    koa example was added.

---
+++ examples/koa-example/app.js
@@ -0,0 +1,33 @@
+var koa = require('koa');
+var app = koa();
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+var middleware = session({
+  store: new FileStore,
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+});
+
+app.use(function *(next) {
+  yield middleware.bind(null, this.req, this.res);
+  yield next;
+});
+
+app.use(function *() {
+  if (this.req.session.views) {
+    this.req.session.views++;
+    this.body = '<p>views: ' + this.req.session.views + '</p>';
+  } else {
+    this.req.session.views = 1;
+    this.body = 'Welcome to the file session demo. Refresh page!';
+  }
+});
+
+var server = app.listen(1337, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
